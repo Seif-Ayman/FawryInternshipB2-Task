ca1c1fef9df247c7b453992f32560540a86050ef
Seif-Ayman <sifeosten5055@gmail.com>
2025-07-08 16:38:16 +0300

    Add Models files that includes 4 classes, the book which is the parent class and the other 3 will be derived from it.

---
+++ FawryInternshipB2 Task/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FawryInternshipB2_Task
+{
+    internal class Inventory
+    {
+    }
+}
---
+++ FawryInternshipB2 Task/Models/EBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FawryInternshipB2_Task.Models
+{
+    internal class EBook
+    {
+    }
+}
