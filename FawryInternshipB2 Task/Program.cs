7d842c42abb6852237996d71449c7206817803ca
Seif-Ayman <sifeosten5055@gmail.com>
2025-07-08 16:33:34 +0300

    Add project files.

---
+++ FawryInternshipB2 Task/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FawryInternshipB2_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
