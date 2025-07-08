9876b1e13f39f33e475fc6e755a8869a61879e7d
Seif-Ayman <sifeosten5055@gmail.com>
2025-07-08 17:17:09 +0300

    Add Ebook Class (Inherited) from Base Class Book. Add Ebook File type and exceptions for buying more than 1.

--- FawryInternshipB2 Task/Models/EBook.cs
+++ FawryInternshipB2 Task/Models/EBook.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace FawryInternshipB2_Task.Models
 {
-    internal class EBook
+    public class EBook : Book
     {
+        public string FileType { get; }
+
+        public EBook(string isbn, string title, int year, double price, string author, string fileType)
+            : base(isbn, title, year, price, author)
+        {
+            FileType = fileType;
+        }
+
+        public override double Buy(int quantity, string email, string address)
+        {
+            if (quantity != 1)
+                throw new InvalidOperationException("You can only aquire 1 ebook of each book");
+
+            Console.WriteLine($"Ebook '{Title}' Sent to {email} via MailService.");
+            return Price;
+        }
     }
 }
